fff1bfb66065a33f05866d21f2477e0454b29131
c-chetan <60259860+c-chetan@users.noreply.github.com>
2020-01-29 19:12:11 +0530

    -implementing logic for meetings list

---
+++ JabilMeetingManagementEXC/MeetingAppDataLayer/DAO/MeetingDAO.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using MeetingAppDataLayer.Models;
+using MeetingAppDataLayer.DBContext;
+
+namespace MeetingAppDataLayer.DAO
+{
+    public class MeetingDAO
+    {
+        private readonly AppSettings appSettings;
+
+        public MeetingDAO()
+        {
+            appSettings = new AppSettings();
+        }
+
+        public IEnumerable<Meeting> GetMeetingsList(int Id)
+        {
+            if(Id!=0)
+            {
+                using(MeetDBContext dBContext = new MeetDBContext(MeetDBContext.optionsBld.dbOptions))
+                {
+                    var attendee = dBContext.Set<Attendee>()
+                                                    .Where(x => x.User.UserId == Id).FirstOrDefault();
+
+                    var attendeeMeetings = dBContext.Set<Meeting>()
+                                                    .Where(x => x.MeetingId == attendee.Meeting.MeetingId).ToList();
+                    return attendeeMeetings;
+                }
+            }
+            else
+            {
+                return new List<Meeting>();
+            }
+        }
+    }
+}
